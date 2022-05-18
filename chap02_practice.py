68ee59ef48fcc5faff6622acba49c52254557185
Metior21 <105237463+Metior21@users.noreply.github.com>
2022-05-18 19:55:46 +0530

    Add files via upload

---
+++ chap02_practice.py
@@ -0,0 +1,61 @@
+# Write a Python program to add two numbers.
+
+'''
+a = 5
+b = 6
+print(a+b)
+
+Or
+
+a = 5
+a += 3
+print(a)
+'''
+
+# Write a Python program to find the remainder when a number is divided by Z(Integer).
+'''
+a = 10
+b = 2
+
+z = (a/b)
+
+print ("z =",z)
+'''
+# Check the type of the variable assigned using the input() function.
+'''
+a = input('number')
+a = str(a)
+print(type(a)
+'''
+# Use a comparison operator to find out whether a given variable a is greater than b or not.
+# (Take a=34 and b=80).
+
+'''
+a = 34
+b = 80
+
+print(a>b)
+'''
+
+# Write a Python program to find the average of two numbers entered by the user.
+'''a = input("value 1 :")  # or a = int(input("value 1 :")) 
+b = input("plus value 2 :") # or b = int(input("value 2 :")) 
+a = int(a) # can also skip this step
+b = int(b) # can also skip this step
+avg = (a+b)/2
+print(type(a))
+print(type(b))
+
+print('the average of numbers = ', (avg))
+'''
+
+# Write a Python program to calculate the square of a number entered by the user.
+'''
+a = int(input('Square of Number'))
+b = int(input('how much times'))
+
+square = (a**b)
+
+print ('square of number :', (square))
+'''
+
