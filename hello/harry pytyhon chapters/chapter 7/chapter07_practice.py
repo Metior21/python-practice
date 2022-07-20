11701d9850a3629477691ad0c7e51f3782998e61
Metior21 <105237463+Metior21@users.noreply.github.com>
2022-07-20 17:01:23 +0530

    Add files via upload

---
+++ hello/harry pytyhon chapters/chapter 7/chapter07_practice.py
@@ -0,0 +1,78 @@
+# # Write a program to print the multiplication table of a given number using for loop.
+
+# # ANSWER
+# """ 
+# z = 22
+
+# for x in range(1,11) :
+#     y = x * z
+#     print(y)
+# """    
+
+
+
+# # Write a program to greet all the person names stored in a list l1 and which starts with S.
+# """
+# l1 = ["Harry", "Sohan", "Sachin", "Rahul"]
+# j = " happy b day "
+# check = "S"
+# result = [x for x in l1 if x.lower().startswith(check.lower())]
+# for x in result :
+#  print(j + x)
+# """
+
+# # Attempt problem 1 using a while loop.
+# """
+# x = input("number")
+# j = 1
+# while j < 11 :
+#     print(j * int(x) )
+#     j = j + 1
+# """
+
+# # Write a program to find whether a given number is prime or not.
+
+
+# n = int(input("number "))
+
+# for i in range(2, n):
+#     if n % i == 0:
+#         print("NOT prime no.")
+#         break
+# else :
+#     print("prime")
+
+# # Write a program to find the sum of first n natural numbers using a while loop.
+
+# x = int(input("sum of natural no."))
+
+# while x 
+
+
+
+# # Write a program to calculate the factorial of a given number using for loop.
+
+
+# # Write a program to print the following star pattern.
+# #     *
+
+# #   ***  
+
+# # ***** for n=3
+
+
+# # Write a program to print the following star pattern:
+# # *
+
+# # **
+
+# # *** for n = 3
+
+
+# # Write a program to print the following star pattern:
+# # * * *
+# # *   *             #For n=3
+# # * * * 
+
+
+# # Write a program to print the multiplication table of n using for loop in reversed order.
